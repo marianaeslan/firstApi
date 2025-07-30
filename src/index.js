c1cc03982339874e89f9bc1d23fc398c740d9a8b
marianaeslan <mariana.eslan@gmail.com>
2025-07-30 10:30:27 -0300

    refactor: create helper function for improved redability

--- src/index.js
+++ src/index.js
@@ -22,6 +22,12 @@ const server = http.createServer((request, response) => {
   if (route) {
     request.query = Object.fromEntries(parsedUrl.searchParams.entries());
     request.params = { id };
+
+    response.send = (statusCode, body) => {
+      response.writeHead(statusCode, {'Content-Type': 'application/json'});
+      response.end(JSON.stringify(body));
+    }
+
     route.handler(request, response);
   } else {
     response.writeHead(404, {'Content-Type' : 'text/html'});
